9f6e75e764c657ad42c50d0067e508efacbe8abf
Sup3r-Us3r <maydersonmello@gmail.com>
2023-01-26 15:41:02 -0300

    create routes for mobile

---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import { View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppRoutes } from './app.routes';
+
+const Routes = () => {
+  return (
+    <View className="flex-1 bg-background">
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
+  );
+};
+
+export { Routes };
