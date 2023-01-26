5920c2917c38ee3adc9e3bf0a317f497e7717461
Sup3r-Us3r <maydersonmello@gmail.com>
2023-01-26 15:40:17 -0300

    create HabitsEmpty component for mobile

---
+++ mobile/src/components/HabitsEmpty.tsx
@@ -0,0 +1,21 @@
+import { Text } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+
+const HabitsEmpty = () => {
+  const { navigate } = useNavigation();
+
+  return (
+    <Text className="text-zinc-400 text-base">
+      Você ainda não está monitorando nenhum hábito{' '}
+      <Text
+        className="text-violet-400 text-base underline active:text-violet-500"
+        onPress={() => navigate('new')}
+      >
+        comece cadastrando um.
+      </Text>
+    </Text>
+  );
+};
+
+export { HabitsEmpty };
